773b9046963c7f4c3ab67479c445f07208a75019
Pietro Rupil <rupilpietro@gmail.com>
2025-12-10 20:04:46 +0100

    ho aggiunto 5 immagini

--- prova_sprite.py
+++ prova_sprite.py
@@ -8,12 +8,22 @@ class MyGame(arcade.Window):
         super().__init__(width, height, title)
 
         self.sprite = None
-        self.sprite_sasso=None
+        self.sprite_sasso = None
+        self.sprite_sasso_nero = None
         self.playerSpriteList = arcade.SpriteList()
         self.SassoSpriteList = arcade.SpriteList()
+        
         self.setup()
 
         self.sasso_setup()
+        
+        self.sasso_nero_setup()
+
+        self.pietra_setup()
+
+        self.grande_pietra_setup()
+
+        self.sasso_medio_setup()
 
         self.keys_pressed = {
             'up': False,
@@ -23,14 +33,54 @@ class MyGame(arcade.Window):
         }
 
     def sasso_setup(self):
-        self.sprite= arcade.Sprite("./assets/sasso.png")
+        self.sprite_sasso= arcade.Sprite("./assets/sasso.png")
 
-        self.sprite_sasso.center_x = 200
+        self.sprite_sasso.center_x = 300
         self.sprite_sasso.center_y = 200
-        self.sprite_sasso.scale_x = 0.1
-        self.sprite_sasso.scale_y = 0.1
+        self.sprite_sasso.scale_x = 1
+        self.sprite_sasso.scale_y = 1
 
-        self.SassoSpriteList.append(self.sprite)
+        self.SassoSpriteList.append(self.sprite_sasso)
+
+    def sasso_nero_setup(self):
+        self.sprite_sasso_nero= arcade.Sprite("./assets/sasso_nero.png")
+
+        self.sprite_sasso_nero.center_x = 300
+        self.sprite_sasso_nero.center_y = 200
+        self.sprite_sasso_nero.scale_x = 1.0
+        self.sprite_sasso_nero.scale_y = 1.0
+
+        self.SassoSpriteList.append(self.sprite_sasso_nero)
+
+    def pietra_setup(self):
+        self.sprite_pietra= arcade.Sprite("./assets/pietra.png")
+
+        self.sprite_pietra.center_x = 400
+        self.sprite_pietra.center_y = 200
+        self.sprite_pietra.scale_x = 2.0
+        self.sprite_pietra.scale_y = 2.0
+
+        self.SassoSpriteList.append(self.sprite_pietra)
+
+    def grande_pietra_setup(self):
+        self.sprite_grande_pietra= arcade.Sprite("./assets/grande_pietra.png")
+
+        self.sprite_grande_pietra.center_x = 200
+        self.sprite_grande_pietra.center_y = 200
+        self.sprite_grande_pietra.scale_x = 1
+        self.sprite_grande_pietra.scale_y = 1
+
+        self.SassoSpriteList.append(self.sprite_grande_pietra)
+
+    def sasso_medio_setup(self):
+        self.sprite_sasso_medio= arcade.Sprite("./assets/sasso_medio.png")
+
+        self.sprite_sasso_medio.center_x = 200
+        self.sprite_sasso_medio.center_y = 200
+        self.sprite_sasso_medio.scale_x = 1
+        self.sprite_sasso_medio.scale_y = 1
+
+        self.SassoSpriteList.append(self.sprite_sasso_medio)
 
     def setup(self):
         
