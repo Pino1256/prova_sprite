e545becc5b734b80bcdd34835359dd980ec6fa01
Pietro Rupil <rupilpietro@gmail.com>
2025-12-10 11:37:55 +0100

    aggiunto il moviento

--- prova_sprite.py
+++ prova_sprite.py
@@ -12,13 +12,20 @@ class MyGame(arcade.Window):
 
         self.setup()
 
+        self.keys_pressed = {
+            'up': False,
+            'down': False,
+            'left': False,
+            'right': False
+        }
+
     def setup(self):
         
         self.sprite = arcade.Sprite("./assets/tile0.png")
 
         self.sprite.center_x = 100
         self.sprite.center_y = 100
-        self.sprite.scale_x = 1.0
+        self.sprite.scale_x = 1.5
         self.sprite.scale_y = 1.0
 
         self.playerSpriteList.append(self.sprite)
@@ -29,10 +36,36 @@ class MyGame(arcade.Window):
         self.playerSpriteList.draw()
         
     def on_update(self, deltaTime):
-        self.sprite.center_x += 1
+        speed = 4  # velocità del personaggio
 
-
-
+        if self.keys_pressed['up']:
+            self.sprite.center_y += speed
+        if self.keys_pressed['down']:
+            self.sprite.center_y -= speed
+        if self.keys_pressed['left']:
+            self.sprite.center_x -= speed
+        if self.keys_pressed['right']:
+            self.sprite.center_x += speed
+    
+    def on_key_press(self, key, modifiers):
+        if key in [arcade.key.UP, arcade.key.W]:
+            self.keys_pressed['up'] = True
+        elif key in [arcade.key.DOWN, arcade.key.S]:
+            self.keys_pressed['down'] = True
+        elif key in [arcade.key.LEFT, arcade.key.A]:
+            self.keys_pressed['left'] = True
+        elif key in [arcade.key.RIGHT, arcade.key.D]:
+            self.keys_pressed['right'] = True
+    
+    def on_key_release(self, key, modifiers):
+        if key in [arcade.key.UP, arcade.key.W]:
+            self.keys_pressed['up'] = False
+        elif key in [arcade.key.DOWN, arcade.key.S]:
+            self.keys_pressed['down'] = False
+        elif key in [arcade.key.LEFT, arcade.key.A]:
+            self.keys_pressed['left'] = False
+        elif key in [arcade.key.RIGHT, arcade.key.D]:
+            self.keys_pressed['right'] = False
 
 def main():
     game = MyGame(
